12c6eedbf157e956e1529780d8536cfd2cee4219
sadscv <sadscv@hotmail.com>
2021-10-26 00:16:18 +0800

    sync

--- src/components/LookupPanel/LookupConditions.vue
+++ src/components/LookupPanel/LookupConditions.vue
@@ -4,6 +4,14 @@
       <a-form-item label="教室号">
         <a-input class="w-100px" v-model="conditions.search['classroom_id']" allow-clear />
       </a-form-item>
+      <a-form-item label="选择日期">
+            <a-date-picker
+      format="YYYY-MM-DD"
+      :disabled-date="disabledDate"
+      class="w-140px"
+      v-model="conditions.search['date']"
+    />
+      </a-form-item>
       <a-form-item label="上课时间">
         <a-tree-select v-model="conditions.class_time" class="w-140px" :tree-data="treeData" tree-checkable :show-checked-strategy="SHOW_PARENT" placeholder="Please select" ></a-tree-select>
 <!--        <MultipleCascader  v-model="conditions.search['class_time']" allow-clear />-->
@@ -20,6 +28,8 @@
 <script>
   import {LookupConditionsMixin} from '../../mixins/LookupPanel';
   import {TreeSelect} from "ant-design-vue";
+  import moment from 'moment';
+
   const SHOW_PARENT = TreeSelect.SHOW_PARENT;
 
   export default {
@@ -32,6 +42,28 @@
         value: [],
         SHOW_PARENT,
       };
+    },
+
+    methods: {
+      moment,
+      range(start, end) {
+        const result = [];
+        for (let i=start; i< end ; i++) {
+          result.push(i);
+        }
+        return result;
+      },
+      disabledDate(current) {
+        return current && current < moment().endOf('day');
+      },
+
+      disabledDateTime() {
+      return {
+        disabledHours: () => this.range(0, 24).splice(4, 20),
+        disabledMinutes: () => this.range(30, 60),
+        disabledSeconds: () => [55, 56],
+      };
+    },
     },
 
     created() {
